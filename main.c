a06261d48ab44aaeb39bb7db460f8be2effd14a0
CalinFodor <calinstefanfodor@gmail.com>
2026-08-28 17:50:12 +0300

    Refactored base tokenizer logic

--- main.c
+++ main.c
@@ -8,60 +8,93 @@ typedef enum _TokenizeState{
     DOUBLE_QUOTE
 } TokenizeState;
 
+void add_char(char* s,char c){
+    int l = strlen(s);  
+  
+    s[l] = c;
+  
+    s[l + 1] = '\0';
+}
+
 int tokenize(char line[],char* tokens[]){
     
     int token_index = 0;
-    int token_start = 0;
     TokenizeState current_state = DEFAULT;
+
+    char token[100] = "";
 
     int line_length = strlen(line);
 
-    for(int i=0;i<line_length;i++){
+    
+    int space_encountered = 0;
+
+    for(int i=0;i<line_length;i++)
+    {
         char character = line[i];
         
-        int token_size = i - token_start;
-        
-        if (character == '\'' && current_state == DEFAULT){
+        if (character == '\'' && current_state != SINGLE_QUOTE){
             current_state = SINGLE_QUOTE;
-            token_start++;
             continue;
         }
 
+        if (character == '"' && current_state == DEFAULT){
+            current_state = DOUBLE_QUOTE;
+            continue;
+        }
+
+        int token_length = strlen(token);
         switch (current_state){
             case DEFAULT:
-                if ((character == ' ' || i == line_length - 1) && token_size != 0 ){
-                    tokens[token_index] = (char*)malloc(token_size);
-
-                    strncpy(tokens[token_index],line + token_start,token_size);
-                    tokens[token_index][token_size] = '\0';
-                    token_index++;
-
-                    token_start = token_start + token_size + 1;
-                    token_size = 0;
-                }else if(character == ' ' && token_size == 0){
-                    token_start++;
-                }
+                if((character == ' ' || i == line_length - 1) && token_length!= 0){
+                    tokens[token_index] = (char*)malloc(token_length);
+                    strncpy(tokens[token_index++],token,token_length);
+                    //reset token
+                    token[0] = '\0';
+                }else if(character != ' '){
+                    add_char(token,character);
+                }    
                 break;
 
             case SINGLE_QUOTE:
                 if(character == '\''){
-                    tokens[token_index] = (char*)malloc(token_size);
+                    tokens[token_index] = (char*)malloc(token_length);
+                    strncpy(tokens[token_index++],token,token_length);
 
-                    strncpy(tokens[token_index],line + token_start,token_size);
-                    tokens[token_index][token_size] = '\0';
-                    token_index++;
-
-                    token_start = token_start + token_size + 1;
-                    token_size = 0;
-
+                    //reset token
+                    token[0] = '\0';
                     current_state = DEFAULT;
                 }else if(i == line_length - 1){
                     return -1;
                 }
+                else {
+                    add_char(token,character);
+                }    
+
                 break;
+            case DOUBLE_QUOTE:
+                if(character != ' ' && space_encountered){
+                    space_encountered = 0;
+                }
 
+                if(character == '"'){
+                    tokens[token_index] = (char*)malloc(token_length);
+                    strncpy(tokens[token_index++],token,token_length);
+
+                    //reset token
+                    token[0] = '\0';
+                    current_state = DEFAULT;
+                }else if(i == line_length - 1){
+                    return -1;
+                }else if(character != ' '){
+                    add_char(token,character);
+                }
+                else if(character == ' ' && !space_encountered){
+                    add_char(token,character);
+                    space_encountered = 1;
+                }
+                    
+                break;
         }
-
         
     }
     return token_index;
@@ -75,7 +108,7 @@ void print_tokens(char* tokens[],int n){
     }
 
     for(int i=0;i<n;i++){
-        printf("[%s]",tokens[i]);
+        printf("[%s] ",tokens[i]);
     }
     printf("\n");
 }
