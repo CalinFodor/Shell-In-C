502aa69760016c034e5ef24f33ed46cd46d46786
CalinFodor <calinstefanfodor@gmail.com>
2026-08-28 13:50:19 +0300

    Single quote parsing

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 
 typedef enum _TokenizeState{
     DEFAULT,
-    SINGLEQUOUTE,
-    DOUBLEQUOTE
+    SINGLE_QUOTE,
+    DOUBLE_QUOTE
 } TokenizeState;
 
 int tokenize(char line[],char* tokens[]){
@@ -21,23 +21,59 @@ int tokenize(char line[],char* tokens[]){
         
         int token_size = i - token_start;
         
-        if ((character == ' ' && token_size != 0) || i == line_length - 1 ){
-            tokens[token_index] = (char*)malloc(token_size);
-
-            strncpy(tokens[token_index],line + token_start,token_size);
-            tokens[token_index][token_size] = '\0';
-            token_index++;
-
-            token_start = token_start + token_size + 1;
-            token_size = 0;
-        }else if(character == ' ' && token_size == 0){
+        if (character == '\'' && current_state == DEFAULT){
+            current_state = SINGLE_QUOTE;
             token_start++;
+            continue;
         }
+
+        switch (current_state){
+            case DEFAULT:
+                if ((character == ' ' || i == line_length - 1) && token_size != 0 ){
+                    tokens[token_index] = (char*)malloc(token_size);
+
+                    strncpy(tokens[token_index],line + token_start,token_size);
+                    tokens[token_index][token_size] = '\0';
+                    token_index++;
+
+                    token_start = token_start + token_size + 1;
+                    token_size = 0;
+                }else if(character == ' ' && token_size == 0){
+                    token_start++;
+                }
+                break;
+
+            case SINGLE_QUOTE:
+                if(character == '\''){
+                    tokens[token_index] = (char*)malloc(token_size);
+
+                    strncpy(tokens[token_index],line + token_start,token_size);
+                    tokens[token_index][token_size] = '\0';
+                    token_index++;
+
+                    token_start = token_start + token_size + 1;
+                    token_size = 0;
+
+                    current_state = DEFAULT;
+                }else if(i == line_length - 1){
+                    return -1;
+                }
+                break;
+
+        }
+
+        
     }
     return token_index;
 }
 
 void print_tokens(char* tokens[],int n){
+
+    if (n == -1){
+        printf("ERR unterminated quote.\n");
+        return;
+    }
+
     for(int i=0;i<n;i++){
         printf("[%s]",tokens[i]);
     }
