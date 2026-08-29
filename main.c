3c0d299b184a637be3acf9fa32ce7372326fe991
CalinFodor <calinstefanfodor@gmail.com>
2026-08-29 10:48:25 +0300

    Almost finished tests

--- main.c
+++ main.c
@@ -21,12 +21,12 @@ int tokenize(char line[],char* tokens[]){
     int token_index = 0;
     TokenizeState current_state = DEFAULT;
 
-    char token[100] = "";
+    char token[128] = {'\0'};
 
     int line_length = strlen(line);
 
-    
     int space_encountered = 0;
+    int escaped = 0;
 
     for(int i=0;i<line_length;i++)
     {
@@ -45,20 +45,34 @@ int tokenize(char line[],char* tokens[]){
         int token_length = strlen(token);
         switch (current_state){
             case DEFAULT:
+                if(character == '\\'){
+                   char next_char = line[i+1];
+                   add_char(token,next_char);
+                   i++;
+                   continue; 
+                }
+
+                if(character != ' '){
+                    add_char(token,character);
+                    token_length++;
+                }    
+
                 if((character == ' ' || i == line_length - 1) && token_length!= 0){
                     tokens[token_index] = (char*)malloc(token_length);
-                    strncpy(tokens[token_index++],token,token_length);
+                    strncpy(tokens[token_index],token,token_length);
+                    tokens[token_index++][token_length] = '\0';
                     //reset token
                     token[0] = '\0';
-                }else if(character != ' '){
-                    add_char(token,character);
-                }    
+                }
+
                 break;
 
             case SINGLE_QUOTE:
                 if(character == '\''){
+                    if(token_length == 0) token_length = 1;
+
                     tokens[token_index] = (char*)malloc(token_length);
-                    strncpy(tokens[token_index++],token,token_length);
+                    strncpy(tokens[token_index++],token,token_length);        
 
                     //reset token
                     token[0] = '\0';
@@ -77,6 +91,9 @@ int tokenize(char line[],char* tokens[]){
                 }
 
                 if(character == '"'){
+
+                    if(token_length == 0) token_length = 1;
+
                     tokens[token_index] = (char*)malloc(token_length);
                     strncpy(tokens[token_index++],token,token_length);
 
@@ -85,30 +102,37 @@ int tokenize(char line[],char* tokens[]){
                     current_state = DEFAULT;
                 }else if(i == line_length - 1){
                     return -1;
-                }else if(character != ' '){
+                }else if(character == '\\'){
+                    char next_char = line[i+1];
+                    add_char(token,next_char);
+                    i++;
+                }
+                else if(character != ' '){
                     add_char(token,character);
                 }
                 else if(character == ' ' && !space_encountered){
                     add_char(token,character);
                     space_encountered = 1;
                 }
-                    
                 break;
         }
-        
     }
+
     return token_index;
 }
 
 void print_tokens(char* tokens[],int n){
 
     if (n == -1){
-        printf("ERR unterminated quote.\n");
+        printf("ERR unterminated quote\n");
         return;
     }
 
     for(int i=0;i<n;i++){
-        printf("[%s] ",tokens[i]);
+        printf("[%s]",tokens[i]);
+        if (i != n-1){
+            printf(" ");
+        }
     }
     printf("\n");
 }
@@ -128,7 +152,6 @@ void tokenize_line(char line[]){
 
 int main(void) {
     char line[1024];
-    printf("Put commands for tokenization\n");
     while (fgets(line, sizeof line, stdin)) {
         if (line[0] == '\n' || line[0] == 0) continue;
         tokenize_line(line);
