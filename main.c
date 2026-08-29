d0d79c224fa85b1e0c2ae70edc1ef26a9b15cdec
CalinFodor <calinstefanfodor@gmail.com>
2026-08-29 20:11:53 +0300

    Finished lesson 2

--- main.c
+++ main.c
@@ -2,157 +2,115 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum _TokenizeState{
-    DEFAULT,
-    SINGLE_QUOTE,
-    DOUBLE_QUOTE
-} TokenizeState;
+void add_char(char *s, char c) {
 
-void add_char(char* s,char c){
-    int l = strlen(s);  
+    while (*s++);
   
-    s[l] = c;
+    *(s - 1) = c;
   
-    s[l + 1] = '\0';
+    *s = '\0';
 }
 
-int tokenize(char line[],char* tokens[]){
-    
-    int token_index = 0;
-    TokenizeState current_state = DEFAULT;
+void add_string(char* strings[],char* string,int poz){
+    int s_len = strlen(string) + 1;
+    strings[poz] = (char*)malloc(s_len);
+    strncpy(strings[poz],string,s_len-1);
+    strings[poz][s_len - 1] = '\0';
+}
 
+int tokenize_pipeline(char line[], char *tokens[])
+{
     char token[128] = {'\0'};
+    int token_index = 0;
 
     int line_length = strlen(line);
+    int in_token = 0;
 
-    int space_encountered = 0;
-    int escaped = 0;
-
-    for(int i=0;i<line_length;i++)
-    {
+    for(int i=0;i<line_length;i++){
         char character = line[i];
-        
-        if (character == '\'' && current_state != SINGLE_QUOTE){
-            current_state = SINGLE_QUOTE;
-            continue;
+
+        if(character == '|'){
+            if(in_token){
+                add_string(tokens,token,token_index);
+                token_index++;
+                token[0] = '\0';
+            }
+
+            add_string(tokens,"|",token_index);
+            token_index++;
+            in_token=0;
+        }else if(character != ' '){
+            add_char(token,character);
+            in_token = 1;
+        }else if(in_token){
+            add_string(tokens,token,token_index);
+            token_index++;
+            token[0] = '\0';
+            in_token = 0;
         }
 
-        if (character == '"' && current_state == DEFAULT){
-            current_state = DOUBLE_QUOTE;
-            continue;
-        }
-
-        int token_length = strlen(token) + 1;
-        switch (current_state){
-            case DEFAULT:
-                if(character == '\\'){
-                   char next_char = line[i+1];
-                   add_char(token,next_char);
-                   i++;
-                   continue; 
-                }
-
-                if(character != ' ' && character != '\n'){
-                    add_char(token,character);
-                    token_length++;
-                }    
-
-                if((character == ' ' || i == line_length - 1) && token_length!= 1){
-                    tokens[token_index] = (char*)malloc(token_length);
-                    strncpy(tokens[token_index],token,token_length);
-                    tokens[token_index++][token_length - 1] = '\0';
-                    //reset token
-                    token[0] = '\0';
-                }
-
-                break;
-
-            case SINGLE_QUOTE:
-                if(character == '\''){
-                    tokens[token_index] = (char*)malloc(token_length);
-                    strncpy(tokens[token_index],token,token_length);        
-                    tokens[token_index++][token_length - 1] = '\0';
-
-                    //reset token
-                    token[0] = '\0';
-                    current_state = DEFAULT;
-                }else if(i == line_length - 1){
-                    return -1;
-                }
-                else {
-                    add_char(token,character);
-                }    
-
-                break;
-            case DOUBLE_QUOTE:
-                if(character != ' ' && space_encountered){
-                    space_encountered = 0;
-                }
-
-                if(character == '"'){
-                    tokens[token_index] = (char*)malloc(token_length);
-                    strncpy(tokens[token_index],token,token_length);
-                    tokens[token_index++][token_length - 1] = '\0';
-
-                    //reset token
-                    token[0] = '\0';
-                    current_state = DEFAULT;
-                }else if(i == line_length - 1){
-                    return -1;
-                }else if(character == '\\'){
-                    char next_char = line[i+1];
-                    add_char(token,next_char);
-                    i++;
-                }
-                else if(character != ' '){
-                    add_char(token,character);
-                }
-                else if(character == ' ' && !space_encountered){
-                    add_char(token,character);
-                    space_encountered = 1;
-                }
-                break;
+        if(i == line_length - 1 && in_token){
+            add_string(tokens,token,token_index++);
         }
     }
-
     return token_index;
 }
 
-void print_tokens(char* tokens[],int n){
+int is_pipe(char* token){
+    if (strcmp(token,"|") == 0)
+        return 1;
+    return 0;
+}
 
+int validate_pipeline(char* tokens[],int n){
+    for(int i=0;i<n;i++){
+        if((i == 0 || i == n-1) && is_pipe(tokens[i])){
+            return -1;
+        }
+        if(is_pipe(tokens[i])){
+            if(is_pipe(tokens[i - 1]) || is_pipe(tokens[i+1])){
+                return -1;
+            }
+        }
+    }
+    return n;
+}
+
+void print_tokens(char* tokens[],int n){
     if (n == -1){
-        printf("ERR unterminated quote\n");
+        printf("ERR syntax error: empty command in pipeline\n");
         return;
     }
 
     for(int i=0;i<n;i++){
-        printf("[%s]",tokens[i]);
-        if (i != n-1){
+        printf("%s",tokens[i]);
+        if(i != n - 1)
             printf(" ");
-        }
     }
     printf("\n");
 }
 
 void free_tokens(char* tokens[],int n){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
         free(tokens[i]);
-    }
 }
 
-void tokenize_line(char line[]){
+void parse_pipeline(char line[]){
     char* tokens[100];
-    int n = tokenize(line,tokens);
+    int n = tokenize_pipeline(line,tokens);
+    n = validate_pipeline(tokens,n);
     print_tokens(tokens,n);
     free_tokens(tokens,n);
 }
 
-int main(void) {
+int main(void)
+{
     char line[1024];
-    while (fgets(line, sizeof line, stdin)) {
-        if (line[0] == '\n' || line[0] == 0) continue;
-        tokenize_line(line);
-
+    while (fgets(line, sizeof line, stdin))
+    {
+        if (line[0] == '\n' || line[0] == 0)
+            continue;
+        parse_pipeline(line);
     }
     return 0;
 }
