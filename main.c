28411b45dd37c388593fa76cb9ea8ae3509037b8
CalinFodor <calinstefanfodor@gmail.com>
2026-08-31 12:02:21 +0300

    Preventive commmit for lesson 4

--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-
-//----TOKENIZER-----
  
 void add_char(char *s, char c)
 {
@@ -29,219 +27,146 @@ void copy_string(char **dest, char *src)
     (*dest)[s_len - 1] = '\0';
 }
 
-int tokenize_redirects(char line[], char *tokens[])
-{
-    char token[128] = {'\0'};
-    int token_index = 0;
+typedef enum _Redir{
+    None,
+    Arrow,
+    LineArrow
+}Redir;
 
-    int line_length = strlen(line);
-    int in_token = 0;
+Redir parse_cmd(char* cmd,int* term_idx){
 
-    for (int i = 0; i < line_length; i++)
-    {
+    int cmd_len = strlen(cmd);
+    Redir redir = None;
+
+    for(int i=0;i<cmd_len;i++){
+        char character = cmd[i];
+
+        if(character == '<' && i != cmd_len - 1){
+            char next_character = cmd[i+1];
+            if(next_character == '<' && i < cmd_len - 3){
+                char next_next_character = cmd[i+2];
+
+                if(next_next_character == '-'){
+                    redir = LineArrow;
+                    *term_idx = i + 3;
+                }else{
+                    redir = Arrow;
+                    *term_idx = i+2;
+                }
+                break;
+            }
+        }
+    }
+    return redir;
+
+}
+
+void extract_terminator(char* line,int term_idx,char term[]){
+    int s_len = strlen(line);
+    int found_sep = 0;
+    for(int i=term_idx;i<s_len;i++){
         char character = line[i];
 
-        if (isdigit(character) && !in_token && i != line_length - 1)
-        {
-            char next_character = line[i + 1];
-            if (next_character == '>' || next_character == '<')
-            {
-                add_char(token, character);
-                add_char(token, next_character);
-                int i_moves = 1;
+        if(character == '\'' || character == '"'){
+            if(found_sep){
+                break;
+            }
+            found_sep = 1;
+        }else  {
+            add_char(term,character);
+        }
+    }
+}
 
-                if (i <= line_length - 3)
-                {
-                    char next_next_character = line[i + 2];
-                    if (next_character == next_next_character)
-                    {
-                        add_char(token, next_character);
-                        i_moves++;
-                    }
+void trim_leading_tabs(char* s){
+    int i=0,j=0;
+
+    while(s[i] == '\t') i++;
+
+    while(s[j++] = s[i++]);
+
+}
+
+void parse_here_docs(char* lines[],int n){
+
+    Redir redir = None;
+    char term[128] = {'\0'};
+    int term_idx = 0;
+
+    for(int i=0;i<n;i++){
+        char* line = lines[i];
+        
+        if(redir == None){
+            redir = parse_cmd(line,&term_idx);
+            printf("CMD %s\n",line);
+            printf("BODY:\n");
+        }
+        
+        switch(redir){
+            case None:    
+                printf("END\n");
+                break;
+            case Arrow:
+                if(strlen(term) == 0){
+                    extract_terminator(line,term_idx,term);
+                    continue;
                 }
 
-                add_string(tokens, token, token_index++);
-                i += i_moves;
-                token[0] = '\0';
-                continue;
-            }
-        }
-
-        if (character == '>' || character == '<')
-        {
-            if (in_token)
-            {
-                add_string(tokens, token, token_index);
-                token_index++;
-                token[0] = '\0';
-                in_token = 0;
-            }
-
-            add_char(token, character);
-
-            if (i != line_length - 1)
-            {
-                char next_character = line[i + 1];
-                if (character == next_character)
-                {
-                    add_char(token, character);
-                    i++;
+                if(strcmp(term,line) == 0){
+                    printf("END\n");
+                    redir = None;
+                }else{
+                    printf("%s\n",line);
                 }
-            }
+                
+                break;
+            case LineArrow:
+                if(strlen(term) == 0){
+                    extract_terminator(line,term_idx,term);
+                    trim_leading_tabs(term);
+                    continue;
+                }
 
-            add_string(tokens, token, token_index++);
-            token[0] = '\0';
-        }
-        else if (character != ' ')
-        {
-            add_char(token, character);
-            in_token = 1;
-        }
-        else if (in_token)
-        {
-            add_string(tokens, token, token_index);
-            token_index++;
-            token[0] = '\0';
-            in_token = 0;
-        }
+                if(strcmp(term,line) == 0){
+                    printf("END\n");
+                    redir = None;
+                }else{
+                    trim_leading_tabs(line);
+                    printf("%s\n",line);
+                }
 
-        if (i == line_length - 1 && in_token)
-        {
-            add_string(tokens, token, token_index++);
+                break;
         }
     }
-    return token_index;
+
 }
 
-//-----PARSER-----
-
-int starts_with_digit(char *token)
-{
-    if (isdigit(token[0]))
-        return 1;
-    return 0;
-}
-
-int has_arrows(char *token, int idx)
-{
-    if ((token[idx] == '>' || token[idx] == '<') &&  token[idx + 1] == '\0') return 1;
-    if((token[idx] == '>' || token[idx] == '<') && (token[idx + 1] == token[idx] && token[idx + 2] == '\0')) return 1;
-    return 0;
-}
-
-int is_redirect_operator(char *token)
-{
-    if (starts_with_digit(token) && has_arrows(token, 1)) return 1;
-    if (has_arrows(token,0)) return 1;
-    return 0;
-}
-
-typedef struct _Redirect
-{
-    char *stream;
-    char *operation;
-    char *target;
-} Redirect;
-
-Redirect new_redirect(char* redir_operator,char* target)
-{
-    Redirect redir;
-
-    char stream[2];
-    stream[1] = '\0';
-
-    int arrow_start = 0;
-
-    if(starts_with_digit(redir_operator)){
-        stream[0] = redir_operator[0];
-        arrow_start = 1;
-    }else if(redir_operator[0] == '<'){
-        stream[0] = '0';
-    }else{
-        stream[0] = '1';
-    }
-
-    copy_string(&redir.stream,stream);
-    copy_string(&redir.operation, redir_operator + arrow_start);
-    copy_string(&redir.target, target);
-
-    return redir;
-}
-
-void print_tokens(char* tokens[],int n){
+void free_strings(char* strings[],int n){
     for(int i=0;i<n;i++)
-        printf("[%s] ",tokens[i]);
-    printf("\n");
-}
-
-void free_tokens(char* tokens[],int n){
-    for(int i=0;i<n;i++)
-        free(tokens[i]);
-}
-
-void parse_redirects(char *tokens[], int n)
-{
-    char *argv[128];
-    int argv_index = 0;
-
-    Redirect redirects[128];
-    int redirects_index = 0;
-
-    int i;
-    for (i = 0; i < n; i++)
-    {        
-        if(is_redirect_operator(tokens[i])){
-            if(i == n - 1){
-                printf("[ERROR] Expected filename\n");
-                return;
-            }
-            Redirect redir = new_redirect(tokens[i],tokens[i+1]);
-            redirects[redirects_index++] = redir;
-            i++;
-        }else{
-            add_string(argv,tokens[i],argv_index++);
-        }
-    }
-
-    printf("argv=[");
-
-    for(int i=0;i<argv_index;i++){
-        if(i != argv_index - 1){
-            printf("'%s', ",argv[i]);
-        }else{
-            printf("'%s']\n",argv[i]);
-        }
-        free(argv[i]);
-    }
-
-    for(int i=0;i<redirects_index;i++){
-       printf("redir fd=%s op=%s target=%s\n",redirects[i].stream,redirects[i].operation,redirects[i].target);
-       free(redirects[i].operation);
-       free(redirects[i].stream);
-       free(redirects[i].target);
-    }
-
-    free_tokens(tokens,n);
-}
-
-
-void process_redirects(char line[])
-{
-    char *tokens[100];
-    int n = tokenize_redirects(line, tokens);
-    parse_redirects(tokens,n);
+        free(strings[i]);
 }
 
 int main(void)
 {
     char line[1024];
+    char *lines[1024];
+    int nolines = 0;
+
     while (fgets(line, sizeof line, stdin))
     {
-        if (line[0] == '\n' || line[0] == 0)
+       printf("%s",line);
+       if(line[0] == '\n') {
+            parse_here_docs(lines,nolines);
+            free_strings(lines,nolines);
+            nolines = 0;
             continue;
-        if(line[strlen(line) - 1] == '\n')
+       }
+       
+       if(line[strlen(line) - 1] == '\n')
             line[strlen(line) - 1] = '\0';
-        process_redirects(line);
+        
+       copy_string(&lines[nolines++],line); 
     }
+
     return 0;
 }
