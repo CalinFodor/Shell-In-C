b13efa8d01aba3405cca2ce8da083a8d21cc0605
CalinFodor <calinstefanfodor@gmail.com>
2026-09-06 14:00:29 +0300

    Added better ui and minor struct fixes

--- functions/ui.c
+++ functions/ui.c
@@ -13,3 +13,25 @@ void print_prompt(){
 
     printf(GRN "%s"RESET"@"YEL"LittleShell" RESET ":" BLU"%s" RESET "$ ",user,cwd);
 }
+
+void print_char_n(char c,int n){
+    for(int i=0;i<n;i++)
+        printf("%c",c);
+    printf("\n");
+}
+
+void print_welcome(){
+    printf("\n");
+    print_char_n('=',WELCOME_LEN);
+
+    printf(".____    .__  __    __  .__           _________.__           .__  .__   \n");
+    printf("|    |   |__|/  |__/  |_|  |   ____  /   _____/|  |__   ____ |  | |  |  \n");
+    printf("|    |   |  \\   __\\   __\\  | _/ __ \\ \\_____  \\ |  |  \\_/ __ \\|  | |  |  \n");
+    printf("|    |___|  ||  |  |  | |  |_\\  ___/ /        \\|   Y  \\  ___/|  |_|  |__\n");
+    printf("|_______ \\__||__|  |__| |____/\\___  >_______  /|___|  /\\___  >____/____/\n");
+    printf("        \\/                        \\/        \\/      \\/     \\/           \n");
+
+    print_char_n('=',WELCOME_LEN);
+    printf("\n");
+}
+
